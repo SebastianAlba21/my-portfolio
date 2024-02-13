818bd7a6b8516ac99476661bdc2050378369ada3
SebastianAlba21 <juan.alba@uao.edu.co>
2024-02-13 10:17:02 -0500

    Cambios para el deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({
-	build: {
-		rollupOptions: {
-			external: ['@/components/sections/Certificates.astro'],
-		},
-	},
-})
+export default defineConfig({})
